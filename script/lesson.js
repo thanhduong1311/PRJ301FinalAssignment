2160e8cbf1187b84e6f09e48999e4b3d4f78ce34
thanhduong1311 <thanhduongjnguyen@gmail.com>
2023-07-19 21:57:16 +0700

    responsive lesson

--- script/lesson.js
+++ script/lesson.js
@@ -283,3 +283,8 @@ function countdown(timer) {
 if (quizTimeRemain.classList.contains("countdown")) {
   countdown(quizTimeRemain.innerHTML);
 }
+
+
+function showMenuList() {
+  document.querySelector(".main > .rightSide").classList.toggle("show")
+}
